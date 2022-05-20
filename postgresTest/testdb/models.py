e1301ed6ad963adc7589a20317489c55e0b20cce
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-05-20 13:57:28 -0400

    running

--- postgresTest/testdb/models.py
+++ postgresTest/testdb/models.py
@@ -2,4 +2,5 @@ from django.db import models
 # Create your models here.
 class Teacher(models.Model):
     name = models.CharField(max_length=80)
+    city = models.CharField(max_length=80)
     age = models.IntegerField()
